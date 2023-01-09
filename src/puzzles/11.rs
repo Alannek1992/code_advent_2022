fce7057bc082c9c29221f0b967a857663eb7b841
Alannek <alesmino92@gmail.com>
2023-01-09 15:24:31 +0100

    eleventh puzzle

--- src/puzzles/11.rs
+++ src/puzzles/11.rs
@@ -11,34 +11,38 @@ pub struct EleventhPuzzle {
 
 impl Solution for EleventhPuzzle {
     fn solution(&self) {
-        print_solution(&self.puzzle.name, self.monkey_business(20), 0);
+        print_solution(
+            &self.puzzle.name,
+            self.monkey_business(20, EleventhPuzzle::division_by_three_compression),
+            0,
+        );
     }
 }
 
 struct Jungle {
-    monkeys: HashMap<i32, Monkey>,
+    monkeys: HashMap<u64, Monkey>,
 }
 
 impl Jungle {
     fn new(monkeys: Vec<Monkey>) -> Self {
         let mut monkeys_as_map = HashMap::new();
         for (idx, monkey) in monkeys.into_iter().enumerate() {
-            monkeys_as_map.insert(idx as i32, monkey);
+            monkeys_as_map.insert(idx as u64, monkey);
         }
         Self {
             monkeys: monkeys_as_map,
         }
     }
 
-    fn play_the_game(&mut self, rounds: i32) {
+    fn play_the_game(&mut self, rounds: u64, compression: Compression) {
         for _ in 0..rounds {
-            for i in 0..self.monkeys.len() as i32 {
+            for i in 0..self.monkeys.len() as u64 {
                 loop {
                     let monkey = match self.monkeys.get_mut(&i) {
                         Some(m) => m,
                         None => break,
                     };
-                    let (item, receiver) = match monkey.throw_to_another_monkey() {
+                    let (item, receiver) = match monkey.throw_to_another_monkey(compression) {
                         Some(t) => t,
                         None => break,
                     };
@@ -49,17 +53,50 @@ impl Jungle {
             }
         }
     }
+
+    fn monkey_business(&self, rounds: u64) -> u64 {
+        jungle.play_the_game(rounds, compression);
+        let mut monkey_activities: Vec<u64> = jungle
+            .monkeys
+            .into_values()
+            .map(|m| m.inspect_count)
+            .sorted()
+            .collect();
+
+        monkey_activities.pop().unwrap() * monkey_activities.pop().unwrap()
+    }
+
+    fn division_by_three_compression(&self, input: u64) -> u64 {
+        input / 3
+    }
+
+    fn magical_compression(&self, _: u64) -> u64 {
+        self.monkeys.values().map(|x| x.test.divisor).product()
+    }
+}
+
+enum CompressionStrategy<'t> {
+    Division(u64),
+    Magic(&'t HashMap<u64, Monkey>)
+}
+
+impl CompressionStrategy {
+    fn execute(&self) -> u64 {
+        match self {
+            Self::Division(n) => 
+        }
+    }
 }
 
 struct Monkey {
-    items: Vec<i32>,
+    items: Vec<u64>,
     operation: Operation,
     test: TestDivisable,
-    inspect_count: i32,
+    inspect_count: u64,
 }
 
 impl Monkey {
-    fn new(items: Vec<i32>, operation: Operation, test: TestDivisable) -> Self {
+    fn new(items: Vec<u64>, operation: Operation, test: TestDivisable) -> Self {
         Self {
             items,
             operation,
@@ -68,14 +105,15 @@ impl Monkey {
         }
     }
 
-    fn throw_to_another_monkey(&mut self) -> Option<(i32, i32)> {
+    fn throw_to_another_monkey(&mut self, compression: Compression) -> Option<(u64, u64)> {
         if self.items.is_empty() {
             return None;
         }
         let item = self.items.remove(0);
-        let item = self.operation.execute(item) / 3;
+        let item = self.operation.execute(item);
+        let item = item;
 
-        let remainder = item % self.test.divider;
+        let remainder = item % self.test.divisor;
         let receiver = if remainder == 0 {
             self.test.success_receiver
         } else {
@@ -85,37 +123,39 @@ impl Monkey {
         Some((item, receiver))
     }
 
-    fn catch_new_item(&mut self, item: i32) {
+    fn catch_new_item(&mut self, item: u64) {
         self.items.push(item);
     }
 }
 
+type Compression = fn(input: u64) -> u64;
+
 enum Operation {
-    Plus(i32),
-    Multiply(i32),
-    Power,
+    Plus(u64),
+    Multiply(u64),
+    Square,
 }
 
 impl Operation {
-    fn execute(&self, input: i32) -> i32 {
+    fn execute(&self, input: u64) -> u64 {
         match self {
             Operation::Plus(n) => input + n,
             Operation::Multiply(n) => input * n,
-            Operation::Power => input * input,
+            Operation::Square => input * input,
         }
     }
 }
 
 struct TestDivisable {
-    divider: i32,
-    success_receiver: i32,
-    fail_receiver: i32,
+    divisor: u64,
+    success_receiver: u64,
+    fail_receiver: u64,
 }
 
 impl TestDivisable {
-    fn new(divider: i32, success_receiver: i32, fail_receiver: i32) -> Self {
+    fn new(divisor: u64, success_receiver: u64, fail_receiver: u64) -> Self {
         Self {
-            divider,
+            divisor,
             success_receiver,
             fail_receiver,
         }
@@ -129,19 +169,6 @@ impl EleventhPuzzle {
         }
     }
 
-    fn monkey_business(&self, rounds: i32) -> i32 {
-        let mut jungle = Jungle::new(self.get_monkeys());
-        jungle.play_the_game(rounds);
-        let mut monkey_activities: Vec<i32> = jungle
-            .monkeys
-            .into_values()
-            .map(|m| m.inspect_count)
-            .sorted()
-            .collect();
-
-        monkey_activities.pop().unwrap() * monkey_activities.pop().unwrap()
-    }
-
     fn get_monkeys(&self) -> Vec<Monkey> {
         let mut monkeys = Vec::new();
         let descriptions: Vec<&str> = self
@@ -153,7 +180,7 @@ impl EleventhPuzzle {
         let re_items = Regex::new(r".*Starting items:.*\n").unwrap();
         let re_item_no = Regex::new(r"\d+").unwrap();
         let re_operation = Regex::new(r".*old ([+*]) (old|\d+)").unwrap();
-        let re_test_divider = Regex::new(r".*by (\d+)").unwrap();
+        let re_test_divisor = Regex::new(r".*by (\d+)").unwrap();
         let re_test_true = Regex::new(r".*true.* (\d+)").unwrap();
         let re_test_false = Regex::new(r".*false.* (\d+)").unwrap();
 
@@ -162,28 +189,28 @@ impl EleventhPuzzle {
             let extracted_items_as_str = &re_items.captures(desc).unwrap()[0];
             re_item_no
                 .captures_iter(extracted_items_as_str)
-                .for_each(|m| items.push(*&m[0].parse::<i32>().unwrap()));
+                .for_each(|m| items.push(*&m[0].parse::<u64>().unwrap()));
 
             let operation_captured = re_operation.captures(desc).unwrap();
             let operator = &operation_captured[1].chars().next().unwrap();
             let operation_value = &operation_captured[2];
             let operation = match operator {
-                '+' => match operation_value.parse::<i32>() {
+                '+' => match operation_value.parse::<u64>() {
                     Ok(n) => Operation::Plus(n),
                     Err(_) => unreachable!(),
                 },
-                '*' => match operation_value.parse::<i32>() {
+                '*' => match operation_value.parse::<u64>() {
                     Ok(n) => Operation::Multiply(n),
-                    Err(_) => Operation::Power,
+                    Err(_) => Operation::Square,
                 },
                 _ => unreachable!(),
             };
 
-            let test_divider_captured = re_test_divider.captures(desc).unwrap();
+            let test_divisor_captured = re_test_divisor.captures(desc).unwrap();
             let test_true_captured = re_test_true.captures(desc).unwrap();
             let test_false_captured = re_test_false.captures(desc).unwrap();
             let test = TestDivisable::new(
-                *&test_divider_captured[1].parse().unwrap(),
+                *&test_divisor_captured[1].parse().unwrap(),
                 *&test_true_captured[1].parse().unwrap(),
                 *&test_false_captured[1].parse().unwrap(),
             );
@@ -200,6 +227,17 @@ mod tests {
     use crate::PuzzleInfo;
 
     use super::*;
+
+    #[test]
+    fn monkey_business_high_load() {
+        assert_eq!(
+            2713310158,
+            EleventhPuzzle {
+                puzzle: get_puzzle_info(),
+            }
+            .monkey_business(10000)
+        );
+    }
 
     #[test]
     fn monkey_business() {
