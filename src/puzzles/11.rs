5f572fa4102f7c31152b327cb78e4fb25b91d761
Alannek <alesmino92@gmail.com>
2023-01-11 15:03:05 +0100

    eleventh puzzle

--- src/puzzles/11.rs
+++ src/puzzles/11.rs
@@ -16,14 +16,14 @@ impl Solution for EleventhPuzzle {
 }
 
 struct Jungle {
-    monkeys: HashMap<u64, Monkey>,
+    monkeys: HashMap<u8, Monkey>,
 }
 
 impl Jungle {
     fn new(monkeys: Vec<Monkey>) -> Self {
         let mut monkeys_as_map = HashMap::new();
         for (idx, monkey) in monkeys.into_iter().enumerate() {
-            monkeys_as_map.insert(idx as u64, monkey);
+            monkeys_as_map.insert(idx as u8, monkey);
         }
         Self {
             monkeys: monkeys_as_map,
@@ -32,7 +32,7 @@ impl Jungle {
 
     fn play_the_game(&mut self, rounds: u64) {
         for _ in 0..rounds {
-            for i in 0..self.monkeys.len() as u64 {
+            for i in 0..self.monkeys.len() as u8 {
                 loop {
                     let monkey = match self.monkeys.get_mut(&i) {
                         Some(m) => m,
@@ -61,14 +61,6 @@ impl Jungle {
 
         monkey_activities.pop().unwrap() * monkey_activities.pop().unwrap()
     }
-
-    fn division_by_three_compression(&self, input: u64) -> u64 {
-        input / 3
-    }
-
-    fn magical_compression(&self, _: u64) -> u64 {
-        self.monkeys.values().map(|x| x.test.divisor).product()
-    }
 }
 
 struct Monkey {
@@ -79,23 +71,11 @@ struct Monkey {
 }
 
 impl Monkey {
-    fn new(items: Vec<u64>, operation: Operation, test: TestDivisable) -> Self {
-        let items_as_digits = Vec::new();
-        items.iter().for_each(|x| {
-            let item_as_digits = Vec::new();
-            let x= *x as u8;
-
-            loop {
-                item_as_digits.push(x % 10);
-                x /= 10;
-
-                if x == 0 {
-                    break;
-                }
-                item_as_digits.reverse();
-                items_as_digits.push(item_as_digits);
-            }
-        });
+    fn new(items: Vec<u8>, operation: Operation, test: TestDivisable) -> Self {
+        let mut items_as_digits = Vec::new();
+        items
+            .iter()
+            .for_each(|x| items_as_digits.push(Operation::convert_num_to_digits(*x)));
         Self {
             items: items_as_digits,
             operation,
@@ -104,15 +84,14 @@ impl Monkey {
         }
     }
 
-    fn throw_to_another_monkey(&mut self) -> Option<(Vec<u8>, u64)> {
+    fn throw_to_another_monkey(&mut self) -> Option<(Vec<u8>, u8)> {
         if self.items.is_empty() {
             return None;
         }
         let item = self.items.remove(0);
-        let item = self.operation.execute(item) / 3;
+        let item = self.operation.execute(item);
 
-        let remainder = item % self.test.divisor;
-        let receiver = if remainder == 0 {
+        let receiver = if TestDivisable::is_divisable(&item, self.test.divisor) {
             self.test.success_receiver
         } else {
             self.test.fail_receiver
@@ -127,34 +106,108 @@ impl Monkey {
 }
 
 enum Operation {
-    Plus(u8),
-    Multiply(u8),
+    Plus(Vec<u8>),
+    Multiply(Vec<u8>),
     Square,
 }
 
 impl Operation {
-    fn execute(&self, input: Vec<u8>) -> Vec<u8> {
+    fn convert_num_to_digits(number: u8) -> Vec<u8> {
+        let mut item_as_digits = Vec::new();
+        let mut x = number;
+
+        loop {
+            item_as_digits.push(x % 10);
+            x /= 10;
+
+            if x == 0 {
+                break;
+            }
+        }
+        item_as_digits.reverse();
+        item_as_digits
+    }
+
+    fn execute(&mut self, input: Vec<u8>) -> Vec<u8> {
         match self {
-            Operation::Plus(n) => {input + n},
-            Operation::Multiply(n) => input * n,
-            Operation::Square => input * input,
+            Operation::Plus(digits) => {
+                let mut result = Vec::new();
+                digits.reverse();
+                let mut cloned_input = input.clone();
+                cloned_input.reverse();
+                let mut remainder = 0;
+                for (idx, digit) in digits.iter().enumerate() {
+                    let existing_digit = cloned_input.get(idx);
+                    match existing_digit {
+                        Some(d) => {
+                            let sum = digit + d + remainder;
+                            let correct_digit = sum % 10;
+                            remainder = sum / 10;
+                            result.push(correct_digit);
+                        }
+                        None => {
+                            result.push(digit + remainder);
+                            remainder = 0;
+                        }
+                    }
+                }
+                result.reverse();
+                result
+            }
+            Operation::Multiply(digits) => {
+                digits.reverse();
+                let mut cloned_input = input.clone();
+                cloned_input.reverse();
+                let mut semi_results = Vec::new();
+                for digit in digits.iter() {
+                    let mut semi_result = Vec::new();
+                    let mut remainder = 0;
+
+                    for existing_digit in cloned_input.iter() {
+                        let sum = digit * existing_digit + remainder;
+                        let correct_digit = sum % 10;
+                        remainder = sum / 10;
+                        semi_result.push(correct_digit);
+                    }
+                    if remainder > 0 {
+                        semi_result.push(remainder);
+                    }
+                    semi_result.reverse();
+                    semi_results.push(semi_result);
+                }
+                let result = semi_results
+                    .into_iter()
+                    .reduce(|a, b| Operation::Plus(a).execute(b))
+                    .unwrap();
+
+                result
+            }
+            Operation::Square => Operation::Multiply(input.clone()).execute(input),
         }
     }
 }
 
 struct TestDivisable {
-    divisor: u64,
-    success_receiver: u64,
-    fail_receiver: u64,
+    divisor: u8,
+    success_receiver: u8,
+    fail_receiver: u8,
 }
 
 impl TestDivisable {
-    fn new(divisor: u64, success_receiver: u64, fail_receiver: u64) -> Self {
+    fn new(divisor: u8, success_receiver: u8, fail_receiver: u8) -> Self {
         Self {
             divisor,
             success_receiver,
             fail_receiver,
         }
+    }
+
+    fn is_divisable(number_as_digits: &Vec<u8>, divisor: u8) -> bool {
+        let mut remainder = 0;
+        for digit in number_as_digits.iter() {
+            remainder = (digit + remainder) % divisor;
+        }
+        remainder == 0
     }
 }
 
@@ -185,18 +238,18 @@ impl EleventhPuzzle {
             let extracted_items_as_str = &re_items.captures(desc).unwrap()[0];
             re_item_no
                 .captures_iter(extracted_items_as_str)
-                .for_each(|m| items.push(*&m[0].parse::<u64>().unwrap()));
+                .for_each(|m| items.push(*&m[0].parse::<u8>().unwrap()));
 
             let operation_captured = re_operation.captures(desc).unwrap();
             let operator = &operation_captured[1].chars().next().unwrap();
             let operation_value = &operation_captured[2];
             let operation = match operator {
                 '+' => match operation_value.parse::<u8>() {
-                    Ok(n) => Operation::Plus(n),
+                    Ok(n) => Operation::Plus(Operation::convert_num_to_digits(n)),
                     Err(_) => unreachable!(),
                 },
                 '*' => match operation_value.parse::<u8>() {
-                    Ok(n) => Operation::Multiply(n),
+                    Ok(n) => Operation::Multiply(Operation::convert_num_to_digits(n)),
                     Err(_) => Operation::Square,
                 },
                 _ => unreachable!(),
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,7 @@
 use std::fmt::Display;
 
+use num::{Unsigned, FromPrimitive};
+
 pub fn print_solution<T: Display>(puzzle_name: &str, first_part_result: T, second_part_result: T) {
     println!("{puzzle_name}: Solution for the first part is: {first_part_result}. Solution for the second part is: {second_part_result}.");
 }
