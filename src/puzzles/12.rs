67160ae5cbbe52c794b8d54e53c1d9205cadead4
Alannek <alesmino92@gmail.com>
2023-01-13 13:21:59 +0100

    twelfth puzzle

--- src/puzzles/12.rs
+++ src/puzzles/12.rs
@@ -4,6 +4,47 @@ pub struct TwelfthPuzzle {
     puzzle: PuzzleInfo,
 }
 
+struct Queue<T> {
+    items: Vec<T>,
+}
+
+impl<T> Queue<T> {
+    fn new() -> Self {
+        Self { items: vec![] }
+    }
+
+    fn enqueue(&mut self, item: T) {
+        self.items.push(item);
+    }
+
+    fn dequeue(&mut self) -> Option<T> {
+        if self.items.len() > 0 {
+            return Some(self.items.remove(0));
+        }
+        None
+    }
+}
+
+struct Location {
+    line: u8,
+    col: u8,
+    tag: char,
+}
+
+impl Location {
+    fn new(line: u8, col: u8, tag: char) -> Self {
+        Self { line, col, tag }
+    }
+
+    fn is_destination(&self) -> bool {
+        self.tag == 'E'
+    }
+
+    fn is_start(&self) -> bool {
+        self.tag == 'S'
+    }
+}
+
 impl TwelfthPuzzle {
     pub fn new() -> Self {
         Self {
